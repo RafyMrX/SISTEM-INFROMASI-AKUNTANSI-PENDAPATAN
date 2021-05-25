7fb1f89a795364145d7508b1dd90949afe91f125
Rafi MrX <you@example.com>
2021-05-25 09:22:26 +0700

    last nota pemesanan fix

--- resources/views/public/layout/header.blade.php
+++ resources/views/public/layout/header.blade.php
@@ -18,7 +18,7 @@
 
   <nav class="navbar navbar-expand-lg navbar-light bg-light">
 	<div class="container">
-		<a class="navbar-brand" href="#">SUVIS INDONESIA</a>
+		<a class="navbar-brand" href="{{url('/')}}">SUVIS INDONESIA</a>
   <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
     <span class="navbar-toggler-icon"></span>
   </button>
@@ -26,7 +26,7 @@
   <div class="collapse navbar-collapse" id="navbarSupportedContent">
     <ul class="navbar-nav ml-auto">
       <li class="nav-item active">
-        <a class="nav-link" href="#">Home <span class="sr-only">(current)</span></a>
+        <a class="nav-link" href="{{url('/')}}">Home <span class="sr-only">(current)</span></a>
       </li>
       <li class="nav-item">
         <a class="nav-link" href="#">Cara Memesan</a>
--- resources/views/public/pemesanan/nota.blade.php
+++ resources/views/public/pemesanan/nota.blade.php
@@ -5,8 +5,118 @@
 
 <div class="container">
 	<div class="card-deck">
-		<div class="card">
-			<h5>Suvis Indonesia</h5>
+		<div class="card kotak" style="padding: 20px;">
+			<h4 style="border-bottom: 1px solid #bababa;padding-bottom: 4px;">Suvis Indonesia</h4>
+		<div class="row">
+			<div class="col-md-6">
+				<p style="font-size: 13pt" class="font-weight-bold">Pemesanan</p>
+				<table>
+					<tr>
+						<td><b>ID</b></td>
+						<td></td>
+						<td></td>
+						<td>P01HS12311</td>
+					</tr>
+					<tr>
+						<td><b>Tanggal</b></td>
+						<td></td>
+						<td></td>
+						<td>20-04-2021 07.16</td>
+					</tr>
+				</table>
+			</div>
+				<div class="col-md-3">
+				</div>
+			<div class="col-md-3">
+				<p style="font-size: 13pt" class="font-weight-bold">Konsumen</p>
+				<table >
+					<tr>
+						<td><b>ID</b></td>
+						<td></td>
+						<td></td>
+						<td>K01021</td>
+					</tr>
+					<tr>
+						<td><b>Nama</b></td>
+						<td></td>
+						<td></td>
+						<td>Afif Fathurrahman</td>
+					</tr>
+				</table>
+			</div>
 		</div>
+
+		<p style="font-size: 13pt" class="font-weight-bold text-center">Data Pemesanan</p>
+
+		<div class="row">
+			<div class="col-md-8">
+				<table class="table table-striped">
+				  <tbody>
+				    <tr>
+				      <td>ID Layanan</td>
+				      <td>:</td>
+				      <td class="v">L02</td>
+				    </tr>
+				    <tr>
+				      <td>Nama Layanan</td>
+				      <td>:</td>
+				      <td  class="v">Service AC</td>
+				    </tr>
+				    <tr>
+				      <td>Waktu Kunjungan</td>
+				      <td>:</td>
+				      <td  class="v">21 April 2021 11.00</td>
+				    </tr>
+
+				     <tr>
+				      <td>Alamat</td>
+				      <td>:</td>
+				      <td  class="v">Jl.Tanah Merah Indah 1</td>
+				    </tr>
+
+				     <tr>
+				      <td>Subtotal</td>
+				      <td>:</td>
+				      <td  class="v">Rp.60.000</td>
+				    </tr>
+
+				     <tr>
+				      <td>Biaya Admin</td>
+				      <td>:</td>
+				      <td  class="v">Rp.4.000</td>
+				    </tr>
+
+				     <tr>
+				      <td>Pajak PPN</td>
+				      <td>:</td>
+				      <td  class="v">Rp.6.000</td>
+				    </tr>
+
+				     <tr>
+				      <td><b>Total</b></td>
+				      <td>:</td>
+				      <td  class="v"><b>Rp.70.000</b></td>
+				    </tr>
+				  </tbody>
+				</table>
+			</div>
+
+			<div class="col-md-4">
+				<div class="kotak">
+					<p class="text-center">Silahkan Lakukan Pembayaran <b>Rp. 70.000</b></p>
+
+					<p class="text-center">ke Bank BNI <b>102-13646464-12</b></p>
+
+					<p class="text-center">AN Reza Rahardian</p>
+				</div>
+			</div>
+		</div>
+
+		</div>
+
+
 	</div>
 </div>
+<br>
+<br>
+@endsection
