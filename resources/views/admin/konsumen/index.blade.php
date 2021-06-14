f579028f3943e25553ee68a26bf7f311c3b0a26e
IlyasArrafii <zainularrafii@gmail.cm>
2021-06-14 14:27:02 +0700

    fix tampilan transaksi, pemesanan, konsumen

--- resources/views/admin/konsumen/index.blade.php
+++ resources/views/admin/konsumen/index.blade.php
@@ -9,12 +9,12 @@
         <div class="container-fluid">
             <div class="row mb-2">
                 <div class="col-sm-6">
-                    <h1 class="m-0">Laporan Konsumen</h1>
+                    <h1 class="m-0">Data Konsumen</h1>
                 </div><!-- /.col -->
                 <div class="col-sm-6">
                     <ol class="breadcrumb float-sm-right">
                         <li class="breadcrumb-item"><a href="{{url('/home')}}">Home</a></li>
-                        <li class="breadcrumb-item active">Laporan Konsumen</li>
+                        <li class="breadcrumb-item active">Data Konsumen</li>
                     </ol>
                 </div><!-- /.col -->
             </div><!-- /.row -->
--- resources/views/admin/pemesanan/index.blade.php
+++ resources/views/admin/pemesanan/index.blade.php
@@ -44,83 +44,24 @@
                     <table id="example1" class="table table-bordered table-striped">
                         <thead>
                             <tr>
-                                <th>Rendering engine</th>
-                                <th>Browser</th>
-                                <th>Platform(s)</th>
-                                <th>Engine version</th>
+                                <th>No</th>
+                                <th>ID Pemesanan</th>
+                                <th>ID Konsumen</th>
+                                <th>Waktu Pemesanan</th>
+                                <th>Status</th>
                                 <th>Edit</th>
                             </tr>
                         </thead>
                         <tbody>
                             <tr>
-                                <td>Tasman</td>
-                                <td>Internet Explorer 5.1</td>
-                                <td>Mac OS 7.6-9</td>
                                 <td>1</td>
-                                <td>C</td>
-                            </tr>
-                            <tr>
-                                <td>Tasman</td>
-                                <td>Internet Explorer 5.2</td>
-                                <td>Mac OS 8-X</td>
-                                <td>1</td>
-                                <td>C</td>
-                            </tr>
-                            <tr>
-                                <td>Misc</td>
-                                <td>NetFront 3.1</td>
-                                <td>Embedded devices</td>
-                                <td>-</td>
-                                <td>C</td>
-                            </tr>
-                            <tr>
-                                <td>Misc</td>
-                                <td>NetFront 3.4</td>
-                                <td>Embedded devices</td>
-                                <td>-</td>
-                                <td>A</td>
-                            </tr>
-                            <tr>
-                                <td>Misc</td>
-                                <td>Dillo 0.8</td>
-                                <td>Embedded devices</td>
-                                <td>-</td>
-                                <td>X</td>
-                            </tr>
-                            <tr>
-                                <td>Misc</td>
-                                <td>Links</td>
-                                <td>Text only</td>
-                                <td>-</td>
-                                <td>X</td>
-                            </tr>
-                            <tr>
-                                <td>Misc</td>
-                                <td>Lynx</td>
-                                <td>Text only</td>
-                                <td>-</td>
-                                <td>X</td>
-                            </tr>
-                            <tr>
-                                <td>Misc</td>
-                                <td>IE Mobile</td>
-                                <td>Windows Mobile 6</td>
-                                <td>-</td>
-                                <td>C</td>
-                            </tr>
-                            <tr>
-                                <td>Misc</td>
-                                <td>PSP browser</td>
-                                <td>PSP</td>
-                                <td>-</td>
-                                <td>C</td>
-                            </tr>
-                            <tr>
-                                <td>Other browsers</td>
-                                <td>All others</td>
-                                <td>-</td>
-                                <td>-</td>
-                                <td>U</td>
+                                <td>P0001</td>
+                                <td>K0001</td>
+                                <td>05-April-21, 10:47:30</td>
+                                <td>Sudah Dibayar</td>
+                                <td><a href="" class="btn btn-primary"><i class="fas fa-pencil-alt"></i>Detail</a>
+                                    <a href="" class="btn btn-primary"><i class="fas fa-eye"></i>Lihat Pembayaran</a>
+                                </td>
                             </tr>
                         </tbody>
                     </table>
