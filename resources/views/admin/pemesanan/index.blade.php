b2005072b35c653e8dac9faf060e90f72608c6b8
IlyasArrafii <zainularrafii@gmail.cm>
2021-06-15 09:32:51 +0700

    fix

--- resources/views/admin/pemesanan/index.blade.php
+++ resources/views/admin/pemesanan/index.blade.php
@@ -54,14 +54,84 @@
                         </thead>
                         <tbody>
                             <tr>
+                                <<<<<<< HEAD <td>1</td>
+                                    <td>P0001</td>
+                                    <td>K0001</td>
+                                    <td>05-April-21, 10:47:30</td>
+                                    <td>Sudah Dibayar</td>
+                                    <td><a href="" class="btn btn-primary"><i class="fas fa-pencil-alt"></i>Detail</a>
+                                        <a href="" class="btn btn-primary"><i class="fas fa-eye"></i>Lihat Pembayaran</a>
+                                    </td>
+                                    =======
+                                    <td>Tasman</td>
+                                    <td>Internet Explorer 5.1</td>
+                                    <td>Mac OS 7.6-9</td>
+                                    <td>1</td>
+                                    <td>C</td>
+                            </tr>
+                            <tr>
+                                <td>Tasman</td>
+                                <td>Internet Explorer 5.2</td>
+                                <td>Mac OS 8-X</td>
                                 <td>1</td>
-                                <td>P0001</td>
-                                <td>K0001</td>
-                                <td>05-April-21, 10:47:30</td>
-                                <td>Sudah Dibayar</td>
-                                <td><a href="" class="btn btn-primary"><i class="fas fa-pencil-alt"></i>Detail</a>
-                                    <a href="" class="btn btn-primary"><i class="fas fa-eye"></i>Lihat Pembayaran</a>
-                                </td>
+                                <td>C</td>
+                            </tr>
+                            <tr>
+                                <td>Misc</td>
+                                <td>NetFront 3.1</td>
+                                <td>Embedded devices</td>
+                                <td>-</td>
+                                <td>C</td>
+                            </tr>
+                            <tr>
+                                <td>Misc</td>
+                                <td>NetFront 3.4</td>
+                                <td>Embedded devices</td>
+                                <td>-</td>
+                                <td>A</td>
+                            </tr>
+                            <tr>
+                                <td>Misc</td>
+                                <td>Dillo 0.8</td>
+                                <td>Embedded devices</td>
+                                <td>-</td>
+                                <td>X</td>
+                            </tr>
+                            <tr>
+                                <td>Misc</td>
+                                <td>Links</td>
+                                <td>Text only</td>
+                                <td>-</td>
+                                <td>X</td>
+                            </tr>
+                            <tr>
+                                <td>Misc</td>
+                                <td>Lynx</td>
+                                <td>Text only</td>
+                                <td>-</td>
+                                <td>X</td>
+                            </tr>
+                            <tr>
+                                <td>Misc</td>
+                                <td>IE Mobile</td>
+                                <td>Windows Mobile 6</td>
+                                <td>-</td>
+                                <td>C</td>
+                            </tr>
+                            <tr>
+                                <td>Misc</td>
+                                <td>PSP browser</td>
+                                <td>PSP</td>
+                                <td>-</td>
+                                <td>C</td>
+                            </tr>
+                            <tr>
+                                <td>Other browsers</td>
+                                <td>All others</td>
+                                <td>-</td>
+                                <td>-</td>
+                                <td>U</td>
+                                >>>>>>> f034a29 (nambah data tables)
                             </tr>
                         </tbody>
                     </table>
